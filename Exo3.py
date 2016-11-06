9a2f9996571987c6569c40af60f17ebd79016cdd
marieksido <sido@et.esiea.fr>
2016-11-06 12:10:32 +0100

    Update Exercice n°3

--- Exo3.py
+++ Exo3.py
@@ -3,30 +3,45 @@ class Node:
         self.left = None # is a Node
         self.right = None # is a Node
         self.value = val
+        self.parent = None #is a node
     
+
+
 # ABR : 
 # left.val <= val <= righ.val
 class Tree:
     def __init__(self, node)
         self.root = node
   
-    def add(self.root, element):
-       if self.root = None
-            self.root.value := element
+    def add(self, element):
+       new = Node(element)
+
+       if self.root is None :
+           self.root = new
+        else :
+            node = self.root
+            while True :
+                if element < node.value :
+                    #Go left
+                    if node.left is None :
+                        node.left = new
+                        node.parent = node
+                        break
+                    node = node.left
+                else :
+                    #Go right
+                    if node.right is None :
+                        node.right = new
+                        node.parent = node
+                        break
+                    node = node.right
+            return new
         
-        else 
-        {
-            if self.root.value > element
-                add(self.root.left, element)
-                
-            else
-                add(self.root.right, element)
-        }
         
-    def run(Tree tree, val)
-        if val in Tree
-            return True
-            
-        else
-            return False
-        
+    def run(self, element)
+        node = self.root
+        while node is not None :
+            if element == node.value :
+                return True
+            else :
+                return False
