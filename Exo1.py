a5f319bbe41bc4e45523712412efca2e16bbcc9f
marieksido <sido@et.esiea.fr>
2016-11-06 12:08:09 +0100

    Update Exercice n°1

--- Exo1.py
+++ Exo1.py
@@ -1,28 +1,18 @@
 class Somme :
 
-    def _intit_(run) :
+    def _intit_(self,n) :
+        self.entier = n
+        self.a = 3
+        self.b = 5
+        self.somme = 0
 
-        run.entier = 1
-        run.somme = 0
-        run.last_nb = 0
-        run.Somme = 0
- 
-        while entier != 0
-        {
-            somme += entier % 10
-            entier /= 10
-        }
-    
-        div_3 = [0, 1, 3, 6, 9, 12, 15, 18, 21, 27, 30]
-    
-        if somme in div_3 :
-            run.Somme += run.Somme + somme
-            entier++
-        
-        last_nb = (entier)%10 
-    
-        div_5 = [0, 5]
-    
-        if last_nb in div_5
-            run.Somme += run.Somme + last_nb
-            entier++
+    def run(self) :
+        for n in range (1, self.entier):
+            if self.entier % self.a == 0:
+                self.somme = self.somme + self.entier
+            elif self.entier % self.b == 0:
+                self.somme = self.somme + self.entier
+            elif :
+                print "Non divisible par %i ou %i" %(self.a,self.b)
+
+                
